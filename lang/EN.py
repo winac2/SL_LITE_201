179e02395c443b098fb2401ff90ca8f9722cf478
winac2 <136137968+winac2@users.noreply.github.com>
2026-03-12 10:42:08 +0700

    first commit

---
+++ lang/EN.py
@@ -0,0 +1,15 @@
+TEXT = {
+    "title": "SignMachine",
+    "controls": "CONTROLS",
+    "start_camera": "START CAMERA 📷",
+    "stop_camera": "STOP CAMERA 🚫📷",
+    "speak": "SPEAK OUT 🗣️",
+    "gesture_none": "GESTURE: NONE",
+    "message": "MESSAGE:",
+    
+    "hello": "hello",
+    "is": "is",
+    "khoi": "khoi",
+    "my": "my",
+    "name": "name"
+}
---
+++ lang/VI.py
@@ -0,0 +1,16 @@
+TEXT = {
+    "title": "SignMachine",
+    "controls": "ĐIỀU KHIỂN",
+    "start_camera": "MỞ CAMERA 📷",
+    "stop_camera": "TẮT CAMERA 🚫📷",
+    "speak": "PHÁT ÂM 🗣️",
+    "gesture_none": "CỬ CHỈ: NONE",
+    "message": "TIN NHẮN:",
+    
+    
+    "hello": "xin chào",
+    "is": "là",
+    "khoi": "khôi",
+    "my": "tôi",
+    "name": "tên"
+}
